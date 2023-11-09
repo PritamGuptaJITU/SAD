9dfe4616b771e6196ec33902702c5d0251c81388
PritamGuptaJITU <71538293+PritamGuptaJITU@users.noreply.github.com>
2023-11-09 11:28:52 +0600

    Add files via upload

---
+++ Faculty.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart FYdp Manager</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }
+        img{
+            height: 150px;
+        }
+
+        .navbar {
+            background: #ff8623;
+            color: white;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 20px 60px;
+        }
+
+        .navbar a {
+            text-decoration: none;
+            color: white;
+            margin-right: 20px;
+        }
+
+        .navbar a:hover {
+            color: #cfbaae;
+        }
+
+        .header {
+            background-color: #FAEDDA;
+            /* text-align: center; */
+            padding: 20px 0;
+            font-size: 45px;
+        }
+
+        .container {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            gap: 20px;
+            padding: 20px;
+        }
+
+        .column {
+            flex: 1;
+            background-color: #f0f0f0;
+            text-align: center;
+            padding: 20px;
+            border-radius: 10px;
+        }
+
+        .column img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 10px;
+        }
+
+        .slogan {
+            font-size: 28px;
+        }
+        .logo{
+          height: 15px;
+          width: 80px;
+        }
+        .box1 {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            gap: 20px;
+            padding: 20px;
+        }
+
+        .box2 {
+            flex: 1;
+            background-color: #f0f0f0;
+            text-align: center;
+            padding: 20px;
+            border-radius: 10px;
+        }
+
+        .box2 img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 10px;
+        }
+        .text{
+          text-align: center;
+        }
+        .box3 {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            gap: 20px;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar">
+        <p style = "font-size:35px";>Smart FYdp Manager</p>
+        <div class="navbar-links">
+            <a href="appliedcompanylist.php">Home</a>
+            <a href="finalclearence.php">Applied Student List</a>
+            <a href="finalclearence.php">UG & PG Program</a>
+            <a href="clearence.php">Groups</a>
+            <a href="adminshipment.php">Research</a>
+            <a href="authority.php">Logout</a>
+        </div>
+    </nav>
+    <div class="header">
+   
+    <img style="height: 150px; width: 500px;" src="logouiu.png" alt="logo">
+<div class="text">
+        <p>Department of<br>Computer Science and Engineering</p>
+        <p class="slogan">We evolve with technology</p> 
+</div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="column">
+            <img src="Pathan Sir.jpg" alt="Pathan Sir">
+            <p style="font-size: 25px;"> Al-Sakib Khan Pathan, Ph.D</p>
+        </div>
+        <div class="column">
+            <img src="Farid Sir.jpeg" alt="Farid Sir">
+            <p style="font-size: 25px;">Md. Farid Dewan  Ph.D</p>
+
+        </div>
+        <div class="column">
+            <img src="Shakkor sir.jpg" alt="Shakkor Sir">
+            <p style="font-size: 25px;">  Swakkhar Shatabda Ph.D</p>
+
+        </div>
+        
+        <div class="column">
+            <img src="Huda sir.png" alt="Huda Sir">
+            <p style="font-size: 25px;"> Mohammad Nurul  Huda Ph.D</p>
+
+        </div>
+        
+        <!-- <div class="column">
+            <img src="Salakul sir.jpg" alt="Salakul Sir">
+            <p style="font-size: 25px;"> Salekul  Islam Ph.D.</p>
+
+        </div> -->
+        <div class="column">
+            <img src="Motaharul sir.jpg" alt="Motaharul Sir">
+            <p style="font-size: 25px;">  Md. Motaharul Islam PhD</p>
+
+        </div>
+    </div>
+    <div class="box1">
+
+    <div class="box2">
+    <img src="Mamun sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;"> Khondaker Abdullah Al Mamun Ph.D</p>
+
+    </div>
+    <div class="box2">
+    <img src="Shariar sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;">Mohammad Shahriar  Rahman,  Phd</p>
+
+    </div>
+    <div class="box2">
+    <img src="Sumon sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;"> Suman Ahmmed,</p>
+
+    </div>
+    <div class="box2">
+    <img src="Nahid sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;">  Nahid Hossain,</p>
+
+
+    </div>
+    <div class="box2">
+    <img src="Rahad sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;"> Khan, Rahad</p>
+
+    </div>
+    
+    
+    
+      </div>
+      <div class="box3">
+
+    <div class="box2">
+    <img src="Mamun sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;"> Khondaker Abdullah Al Mamun Ph.D</p>
+
+    </div>
+    <div class="box2">
+    <img src="Shariar sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;">Mohammad Shahriar  Rahman,  Phd</p>
+
+    </div>
+    <div class="box2">
+    <img src="Sumon sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;"> Suman Ahmmed,</p>
+
+    </div>
+    <div class="box2">
+    <img src="Nahid sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;">  Nahid Hossain,</p>
+
+
+    </div>
+    <div class="box2">
+    <img src="Rahad sir.jpg" alt="Sumon Sir">
+    <p style="font-size: 25px;"> Khan, Rahad</p>
+
+    </div>
+    
+    
+    
+      </div>
+</body>
+</html>
